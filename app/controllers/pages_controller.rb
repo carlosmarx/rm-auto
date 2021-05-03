898b13792673f35ff22e4a6535fff5411eecc2bd
Elias Ferreira Junior <eliaswebdev@gmail.com>
2021-05-03 18:11:57 -0300

    ruby 2.7.1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,9 @@
 class PagesController < AdminController
-  
-  def dashboard
-  end
+  def dashboard; end
 
-  def index
-  end
+  def index; end
 
-  def blank
-  end
-  
-  def chat
-  end
-  
+  def blank; end
+
+  def chat; end
 end
